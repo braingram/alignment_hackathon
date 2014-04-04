47d373006c85c6d8f5e987a1af08db54a9b3ca8e
Brett Graham <brettgraham@gmail.com>
2014-04-04 15:54:25 -0400

    adding command line options for run

--- serve/run.py
+++ serve/run.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
+import sys
+
 import server
 
 if __name__ == '__main__':
-    server.run(debug=True)
+    host = 'localhost'
+    port = 5555
+    debug = False
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
+    if len(sys.argv) > 3:
+        debug = True
+    server.run(host=host, port=port, debug=debug)
